d5ba36623d8c7f9780e7e9e185db895ae3260594
genie9 <jenny.lyjina@gmail.com>
2015-05-17 23:58:01 +0300

    aloitettu näkymien työstöä

--- app/controllers/hello_world_controller.php
+++ app/controllers/hello_world_controller.php
@@ -1,14 +1,23 @@
 <?php
 
-  class HelloWorldController extends BaseController{
+class HelloWorldController extends BaseController {
 
-    public static function index(){
-      // make-metodi renderöi app/views-kansiossa sijaitsevia tiedostoja
-   	  View::make('home.html');
+    public static function index() {
+        // make-metodi renderöi app/views-kansiossa sijaitsevia tiedostoja
+        View::make('base.html');
+//        echo 'Hello Universe!';
     }
 
-    public static function sandbox(){
-      // Testaa koodiasi täällä
-      echo 'Hello World!';
+    public static function hallinta() {
+        // Testaa koodiasi täällä
+        View::make('hallinta_base.html');
+//        echo 'Hello World!';
     }
-  }
+
+    public static function sandbox() {
+        // Testaa koodiasi täällä
+        View::make('helloworld.html');
+//        echo 'Hello World!';
+    }
+
+}
