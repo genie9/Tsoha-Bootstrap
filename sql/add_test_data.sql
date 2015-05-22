ef5f3834f91c616e0b629f64fc37f3ad566ac17f
genie9 <jenny.lyjina@gmail.com>
2015-05-22 18:41:44 +0300

    Dokumentaatio päivitetty. Create_tables päivitetty

--- sql/add_test_data.sql
+++ sql/add_test_data.sql
@@ -1,3 +1,4 @@
 INSERT INTO Jasen (nimi, email, syntyma, status) VALUES ('Kira Kallio', 'kira.kallio@gmail.com', '11.11.2011', true);
 INSERT INTO Jasen (nimi, email, syntyma, status) VALUES ('Himo Kiipeilijä', 'himo.kiipeilija@gmail.com', '21.12.2012', true);
 INSERT INTO Hallitus (vuosi) VALUES ('2015');
+INSERT INTO Kokous (pvm, aika, tyyppi, hal_vuosi) VALUES ('23.3.2015', '18:00', 'Perustamiskokous', '2015'); 
--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -7,55 +7,57 @@ CREATE TABLE Jasen (
     puhelin integer,
     syntyma date NOT NULL,
     huoltaja varchar(50),
-    laji varchar[],
+    laji text[],
+    rek_aika timestamp,
     status boolean DEFAULT false,
     skilstatus boolean DEFAULT false,
     seura varchar(20)
 );
 
 CREATE TABLE Hallitus (
-    id SERIAL PRIMARY KEY,
-    vuosi integer NOT NULL
-);
-
-CREATE TABLE Hallitus_has_Jasen (
-    hallitus_id integer NOT NULL,
-    jasen_id integer NOT NULL,
-    PRIMARY KEY (hallitus_id, jasen_id),
-    FOREIGN KEY (hallitus_id) REFERENCES Hallitus
+    /*id SERIAL PRIMARY KEY,*/
+    vuosi integer PRIMARY KEY NOT NULL
 );
 
 CREATE TABLE Kokous (
-    id SERIAL PRIMARY KEY, 
-    pvm date,
+    pvm date PRIMARY KEY NOT NULL,
     aika varchar(5) NOT NULL,
     nimi varchar(50),
-    tyyppi varchar(15) NOT NULL,
-    muuta varchar(200)
+    tyyppi varchar(20) NOT NULL,
+    muuta varchar(200),
+    hal_vuosi integer, 
+    FOREIGN KEY (hal_vuosi) REFERENCES Hallitus
+);
+
+CREATE TABLE Hallitus_has_Jasen (
+    hal_vuosi integer NOT NULL,
+    jasen_id integer NOT NULL,
+    PRIMARY KEY (hal_vuosi, jasen_id),
+    FOREIGN KEY (hal_vuosi) REFERENCES Hallitus
 );
 
 CREATE TABLE Kokous_has_Jasen (
-    kokous_id integer NOT NULL,
+    kokous_pvm date NOT NULL,
     jasen_id integer NOT NULL,
-    PRIMARY KEY (kokous_id, jasen_id),
-    FOREIGN KEY (kokous_id) REFERENCES Kokous
+    PRIMARY KEY (kokous_pvm, jasen_id),
+    FOREIGN KEY (kokous_pvm) REFERENCES Kokous
 );
 
 CREATE TABLE Dokumentti (
     id SERIAL PRIMARY KEY,
     nimi varchar(50) NOT NULL,
     pvm date NOT NULL,
-    url varchar(1000) NOT NULL,
-    kokous_id integer NOT NULL,
-    FOREIGN KEY (kokous_id) REFERENCES Kokous
+    url text NOT NULL,
+    kokous_pvm date NOT NULL,
+    FOREIGN KEY (kokous_pvm) REFERENCES Kokous
 );
 
 CREATE TABLE Jasenmaksu (
     id SERIAL PRIMARY KEY,
-    vuosi date,
-    maara_lapsi numeric,
-    maara_aikuinen numeric,
-    maara_skil numeric
+    vuosi date NOT NULL,
+    maara_lapsi numeric NOT NULL,
+    maara_aikuinen numeric NOT NULL,
+    maara_skil numeric NOT NULL
 );
 
 CREATE TABLE Jasen_has_Jasenmaksu (
